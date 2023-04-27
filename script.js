84176c0e39cdfb144dee3da1526f8a6efe8587cb
MohdSajid8010 <117560964+MohdSajid8010@users.noreply.github.com>
2023-04-27 16:17:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 //your JS code here. If required.
 
-function firstNonRepeatingCharacter(str) {
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
-      return char;
-    }
-  }
+// function firstNonRepeatingCharacter(str) {
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i];
+//     if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
+//       return char;
+//     }
+//   }
   
-}
+// }
 
-// let str=prompt("Enter a string");
-
-let ans=firstNonRepeatingCharacter(str);
-
-alert(ans);
-
+let str=prompt("Enter a string");
 
 function firstNonRepeatedChar(str) {
   let charCount = {};
@@ -33,3 +28,9 @@ function firstNonRepeatedChar(str) {
   }
   return null;
 }
+
+let ans=firstNonRepeatingCharacter(str);
+
+alert(ans);
+
+
